444da5d80656e2ab5ab2054733768485ec2405aa
Luna <lunacristofanelli@gmail.com>
2024-05-17 00:37:16 -0300

    Login Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AdministradorModule } from './administrador/administrador.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AdministradorModule, UsuarioModule, CommonModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
